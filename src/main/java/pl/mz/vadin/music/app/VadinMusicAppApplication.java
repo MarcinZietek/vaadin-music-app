8616b078548b1451899454a1e9c1cfe1ec3e49e4
unknown <marcin.it.zietek@gmail.com>
2022-11-25 13:17:14 +0100

    initial commit

---
+++ src/main/java/pl/mz/vadin/music/app/VadinMusicAppApplication.java
@@ -0,0 +1,13 @@
+package pl.mz.vadin.music.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VadinMusicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VadinMusicAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mz/vadin/music/app/VadinMusicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mz.vadin.music.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VadinMusicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
